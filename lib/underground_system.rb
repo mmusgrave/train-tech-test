2002ada961b155ca26bf687294f76c8069cc3e78
mmusgrave <mmusgrave@pobox.com>
2023-10-29 22:30:15 +0100

    Add errors, required function params, and floats

--- lib/underground_system.rb
+++ lib/underground_system.rb
@@ -1,24 +1,22 @@
 class UndergroundSystem
-  #start with int then make it float
-
   def initialize
     @customers_hash = {}
   end
 
-  def check_in(id, station_name, time)
+  def check_in(id:, station_name:, time:)
     # Create a new customer object in hash with their id as key if one does not exist already
     if !@customers_hash.has_key?(id)
       @customers_hash[id] = { checked_in?: false, trips: [] } # Creates key-value pair for customer
     end
 
     # If the customer is checked in, they can't check in again
-    return "This customer has already been checked in" if @customers_hash[id][:checked_in?] # TODO: error handling
+    raise CheckInError if @customers_hash[id][:checked_in?]
 
     @customers_hash[id][:checked_in?] = true
     @customers_hash[id][:trips] << {start_time: time, start_station: station_name}
   end
 
-  def check_out(id, station_name, time)
+  def check_out(id:, station_name:, time:)
     # The last one on the array should be the open trip # TODO: test this assumption
     current_trip = @customers_hash[id][:trips].last()
 
@@ -27,22 +25,23 @@ class UndergroundSystem
       current_trip[:end_time] = time
       current_trip[:end_station] = station_name
     else
-      # raise
-      return "This customer has no open trips"
+      raise CheckOutError
     end
   end
 
-  def get_average_time(start_station, end_station)
+  def get_average_time(start_station:, end_station:)
+    # Use float values for more precise averages
     valid_trip_lengths = []
-    valid_trip_counter = 0
+    valid_trip_counter = 0.0
     @customers_hash.each do |customer_id, customer_info|
       customer_info[:trips].each do |trip|
         if finished_trip?(trip) && matching_trip?(trip, start_station, end_station)
-          valid_trip_lengths << (trip[:end_time] - trip[:start_time]) # Length of trip
-          valid_trip_counter += 1
+          valid_trip_lengths << (trip[:end_time].to_f - trip[:start_time].to_f) # Length of trip
+          valid_trip_counter += 1.0
         end
       end
     end
+    raise NoValidTripsError if valid_trip_counter == 0
     # Sum up distances and divide by number of trips for average
     return (valid_trip_lengths.sum/valid_trip_counter)
   end
@@ -55,5 +54,23 @@ class UndergroundSystem
 
   def finished_trip?(trip)
     return trip[:end_station] != nil
+  end
+end
+
+class CheckInError < StandardError
+  def message
+    "This customer is already checked in to another station"
+  end
+end
+
+class CheckOutError < StandardError
+  def message
+    "This customer has no open trips from which they can check out"
+  end
+end
+
+class NoValidTripsError < StandardError
+  def message
+    "There have been no trips made between these 2 stations"
   end
 end
